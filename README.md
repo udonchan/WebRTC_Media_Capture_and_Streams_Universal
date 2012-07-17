a8328d374fe029f173aa61aefc33fe4f1c87cdf7
鈴木 Udonchan 督史 <suzuki.masafumi@lab.ntt.co.jp>
2012-07-17 18:27:09 +0900

    Firefox Nightly に対応

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 #getUserMedia-Universal
-最近のOperaとChrome18以降で動作します．大事なことなので繰り返しますがOperaでもChrome18でもChrome20以降でも動きます．
+最近のOperaとChrome18以降とFirefox16(2012年現在nightlyだけ)以降とWebRTCプラギンをインストールしたIEで動作します．
+大事なことなので繰り返しますが事実上全てのブラウザでクロスブラウザしてます．とてもCool．
+
+Firefox で動作させる場合はユーザプロファイルの pref.js に以下の記述を追加する必要があります．（本来であればabout:configから設定可能な筈ですが…）
+
+    user_pref("media.navigator.enabled", true);
 
 インスペクタでWebRTCのMediaStreamの実装状況を手軽にチェックするために作りました．
 
--- script.js
+++ script.js
@@ -3,22 +3,29 @@ var _media_stream;
 
 setTimeout(function(){
     (_video = document.getElementById("video")).play();
-    if ("getUserMedia" in navigator)
+    if("getUserMedia" in navigator)
 	/* for Opera */
         navigator.getUserMedia(
 	    {audio : true, video : true, toString : function(){return "video, audio";}},
             function(s) {
 		_media_stream = _video.src = s;
 	    }, gotStreamFailed);
-    else if ("webkitGetUserMedia" in navigator)
+    else if("webkitGetUserMedia" in navigator)
 	/* for Chrome and IE */
         navigator.webkitGetUserMedia(
 	    {audio : true, video : true, toString : function(){return "video, audio";}},
             function(s) {
                 _video.src = webkitURL.createObjectURL(_media_stream = s);
             }, gotStreamFailed);
+    else if("mozGetUserMedia" in navigator)
+	/* for Firefox 16 or latter */
+	navigator.mozGetUserMedia(
+	    {video: true},
+	    function(s) {
+		_media_stream = _video.src = s;
+	    }, gotStreamFailed);
     else
-        gotStreamFailed("This site rquires WebRTC. Please get Chrome or Opera and set MediaStream available.");
+        gotStreamFailed("This site rquires WebRTC. Please get Chrome or Opera or Firefox nightly build and set MediaStream available.");
 }, 1);
 
 function gotStreamFailed(e){
