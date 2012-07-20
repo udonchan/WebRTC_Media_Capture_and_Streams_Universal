47b5e260f4674e2a8e725a1500dbf29b1ec3b2e0
udonchan <udonchan@udonchan.org>
2012-07-20 21:48:34 +0900

    外部公開用

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#getUserMedia-Universal
+#WebRTC_Media_Capture_and_Streams_Universal
 最近のOperaとChrome18以降とFirefox16(2012年現在nightlyだけ)以降とWebRTCプラギンをインストールしたIEで動作します．
 大事なことなので繰り返しますが事実上全てのブラウザでクロスブラウザしてます．とてもCool．
 
@@ -8,4 +8,3 @@ Firefox で動作させる場合はユーザプロファイルの pref.js に以
 
 インスペクタでWebRTCのMediaStreamの実装状況を手軽にチェックするために作りました．
 
-![ss](/udonchan/getUserMedia-Universal/raw/master/doc/ss.png)
--- script.js
+++ script.js
@@ -9,6 +9,7 @@ setTimeout(function(){
 	    {audio : true, video : true, toString : function(){return "video, audio";}},
             function(s) {
 		_media_stream = _video.src = s;
+		gotStreamSuccess(s);
 	    }, gotStreamFailed);
     else if("webkitGetUserMedia" in navigator)
 	/* for Chrome and IE */
@@ -16,17 +17,23 @@ setTimeout(function(){
 	    {audio : true, video : true, toString : function(){return "video, audio";}},
             function(s) {
                 _video.src = webkitURL.createObjectURL(_media_stream = s);
+		gotStreamSuccess(s);
             }, gotStreamFailed);
     else if("mozGetUserMedia" in navigator)
 	/* for Firefox 16 or latter */
 	navigator.mozGetUserMedia(
-	    {video: true},
+	    {video : true},
 	    function(s) {
 		_media_stream = _video.src = s;
+		gotStreamSuccess(s);
 	    }, gotStreamFailed);
     else
         gotStreamFailed("This site rquires WebRTC. Please get Chrome or Opera or Firefox nightly build and set MediaStream available.");
 }, 1);
+
+function gotStreamSuccess(s){
+
+}
 
 function gotStreamFailed(e){
     alert(e);
